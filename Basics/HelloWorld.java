f00181802ac7b2507a1a12489cd484753137372a
26Vamsi <edaravamsi26@gmail.com>
2024-08-07 19:48:14 +0530

    Changes Done

--- Basics/HelloWorld.java
+++ Basics/HelloWorld.java
@@ -7,3 +7,10 @@ public class HelloWorld {
     }
 
 }
+
+//Basic Output
+//1.System.out.println() -->  It prints the given text and adds a new line at the end
+//2.System.out.print() --> This method prints the text without adding a new line.
+//3.System.out.printf() -->  This method allows formatted output, similar to printf in C.
+//int age = 30;
+//System.out.printf("My age is %d.\n", age);
