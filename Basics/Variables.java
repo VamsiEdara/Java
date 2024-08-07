87d7f61459dfc45866d1684e96a680cdf6b59ef9
26Vamsi <edaravamsi26@gmail.com>
2024-08-07 20:27:14 +0530

    Changes Done

---
+++ Basics/Variables.java
@@ -0,0 +1,34 @@
+package Basics;
+
+public class Variables {
+    public static void main(String[] args) {
+        boolean flag =  true;
+        System.out.println(flag);
+
+        byte range;
+        range = 124;
+        System.out.println(range);
+
+        short temperature;
+        temperature = -200;
+        System.out.println(temperature);
+
+        int ranges = -4250000;
+        System.out.println(ranges);
+        // L
+        long ranges1 = -4234234252452L;
+
+        double number = -42.3;
+        System.out.println(number);
+        // f
+        float numbers = -42.3f;
+        System.out.println(number);
+
+        char letter = '\u0051';
+        System.out.println(letter);
+
+        String str = "Vamsi @";
+
+
+    }
+}
